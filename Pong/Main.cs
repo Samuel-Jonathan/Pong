109e81fb03639ccb1074007ea4c79f25d70cb239
Samuel-Jonathan <jtn.samuel@gmail.com>
2022-06-14 19:15:36 +0200

    Ajoutez des fichiers projet.

---
+++ Pong/Main.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong
+{
+    public class Main : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        //Déclaration des textures
+        private Texture2D background;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            //Dimension de la fenêtre
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+       
+
+        }
+
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Chargement des textures
+            background = Content.Load<Texture2D>("background");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+
+            //Dessine l'arrière-plan
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+          
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
