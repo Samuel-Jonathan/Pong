ebb8a1818b6608ce815dedaa8c3df8ee0dbc3b1a
Samuel-Jonathan <jtn.samuel@gmail.com>
2022-06-15 12:21:30 +0200

    Player

--- Pong/Main.cs
+++ Pong/Main.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Pong
 {
@@ -8,8 +10,16 @@ namespace Pong
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
 
+        //Dimension de la fenêtre
+        private int width = 800;
+        private int height = 600;
+
         //Déclaration des textures
         private Texture2D background;
+        private Texture2D player;
+
+        //Liste des joueurs
+        private List<Player> players = new List<Player>();
 
         public Main()
         {
@@ -18,9 +28,9 @@ namespace Pong
             IsMouseVisible = true;
 
             //Dimension de la fenêtre
-            graphics.PreferredBackBufferWidth = 800;
-            graphics.PreferredBackBufferHeight = 600;
-       
+            graphics.PreferredBackBufferWidth = width;
+            graphics.PreferredBackBufferHeight = height;
+
 
         }
 
@@ -36,11 +46,29 @@ namespace Pong
 
             //Chargement des textures
             background = Content.Load<Texture2D>("background");
+            player = Content.Load<Texture2D>("player");
+
+            //Joueurs
+            Player playerLeft = new Player(player, 40, height / 2, 5);
+            players.Add(playerLeft);
+
+            Player playerRight = new Player(player, 760, height / 2, 5);
+            players.Add(playerRight);
+
 
         }
 
         protected override void Update(GameTime gameTime)
         {
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].Collision();
+            }
+
+
+            players[0].MoveLeft();
+            players[1].MoveRight();
+
 
             base.Update(gameTime);
         }
@@ -51,7 +79,13 @@ namespace Pong
 
             //Dessine l'arrière-plan
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
-          
+            
+            //Dessine les joueurs
+            for(int i = 0; i < players.Count; i++)
+            {
+                players[i].Draw(spriteBatch);
+            }
+
             spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ Pong/Player.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Pong
+{
+    public class Player
+    {
+
+        //Texture
+        private Texture2D texture;
+
+        //Position du joueur
+        private int x;
+        private int y;
+
+        //Vitesse
+        private int speed;
+
+        public Player(Texture2D texture, int x, int y, int speed)
+        {
+            this.texture = texture;
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Vector2(x, y), Color.White);
+        }
+
+        public void MoveLeft()
+        {
+            //Déplacement du joueur (gauche)
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                y -= speed;
+            }else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                y += speed;
+            }
+        }
+
+        public void MoveRight()
+        {
+            //Déplacement du joueur (droit)
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                y -= speed;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                y += speed;
+            }
+        }
+
+        public void Collision()
+        {
+
+            //Coordonnées du haut et bas de la fenêtre
+            int top = 0;
+            int bottom = 472;
+
+            //Collision du joueur
+            if(y <= top)
+            {
+                y += speed;
+            }else if(y >= bottom)
+            {
+                y -= speed;
+            }
+        }
+    }
+}
